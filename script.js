e1166b6e581a2b7450adc3a541116b73dd95b2cb
foryzen <92988541+foryzen@users.noreply.github.com>
2022-09-24 16:07:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+total = 0
+
+function n1(){
+    total = total+680
+    document.getElementById('output').innerHTML = total
+}
+
+function s1(){
+    total = total+650
+    document.getElementById('output').innerHTML = total
+}
+
+function n2(){
+    total = total+700
+    document.getElementById('output').innerHTML = total
+}
+
+function s2(){
+    total = total+670
+    document.getElementById('output').innerHTML = total
+}
+
+function n3(){
+    total = total+680
+    document.getElementById('output').innerHTML = total
+}
+
+function s3(){
+    total = total+650
+    document.getElementById('output').innerHTML = total
+}
+
+function n4(){
+    total = total+680
+    document.getElementById('output').innerHTML = total
+}
+
+function s4(){
+    total = total+650
+    document.getElementById('output').innerHTML = total
+}
+
+function n5(){
+    total = total+660
+    document.getElementById('output').innerHTML = total
+}
+
+function s5(){
+    total = total+630
+    document.getElementById('output').innerHTML = total
+}
+
+function n6(){
+    total = total+640
+    document.getElementById('output').innerHTML = total
+}
+
+function s6(){
+    total = total+610
+    document.getElementById('output').innerHTML = total
+}
+
+function n7(){
+    total = total+520
+    document.getElementById('output').innerHTML = total
+}
+
+function s7(){
+    total = total+490
+    document.getElementById('output').innerHTML = total
+}
+
+function n8(){
+    total = total+580
+    document.getElementById('output').innerHTML = total
+}
+
+function s8(){
+    total = total+550
+    document.getElementById('output').innerHTML = total
+}
+
+
+
+function h3(){
+    total = total+580
+    document.getElementById('output').innerHTML = total
+}
+
+function h4(){
+    total = total+580
+    document.getElementById('output').innerHTML = total
+}
+
+function h6(){
+    total = total+540
+    document.getElementById('output').innerHTML = total
+}
+
+
+
+function c1(){
+    total = total-100
+    document.getElementById('output').innerHTML = total
+}
+
+function c2(){
+    total = total-200
+    document.getElementById('output').innerHTML = total
+}
+
+function c3(){
+    total = total-300
+    document.getElementById('output').innerHTML = total
+}
